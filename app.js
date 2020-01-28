06f7aad888d352535a555b560fb69ebbfc1e2cd2
sskywalker <hkrai39@gmail.com>
2020-01-28 12:56:02 +0530

    created a component and rendered it by passing the props value

---
+++ app.js
@@ -0,0 +1,10 @@
+
+Vue.component("cardie",{
+    // Here it is basically using id of template, we can even pass specific html elements to in template like `<h3>This template </h3>`
+    template: '#card-template',
+    props:['cardValue']
+
+})
+new Vue({
+    el:"#app"
+})
