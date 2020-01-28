2863c59e78e5f18e8970966ffae73ecea304894e
sskywalker <hkrai39@gmail.com>
2020-01-28 13:31:04 +0530

    small changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 Vue.component("cardie",{
-    // Here it is basically using id of template, we can even pass specific html elements to in template like `<h3>This template </h3>`
+    // Here it is basically using id of template, we can even pass specific html elements in template like `<h3>This template </h3> but for linking we need to give 'is' property to the template`
     template: '#card-template',
     props:['cardValue']
 
